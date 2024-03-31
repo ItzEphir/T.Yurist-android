22a430f66370e3e7d58f2fa7c222028e962757fb
Ephir <d.y.dvoryannikov@mail.ru>
2024-04-01 00:15:44 +0300

    feat: Add layout for AddMeetScreen

--- app/src/main/java/ru/sber_tech/prod_mobile/components/SegmentedButtonSelect.kt
+++ app/src/main/java/ru/sber_tech/prod_mobile/components/SegmentedButtonSelect.kt
@@ -5,13 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.InputChip
-import androidx.compose.material3.SegmentedButton
-import androidx.compose.material3.SegmentedButtonDefaults
-import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
--- app/src/main/java/ru/sber_tech/prod_mobile/utils/DateUtils.kt
+++ app/src/main/java/ru/sber_tech/prod_mobile/utils/DateUtils.kt
@@ -1,7 +1,5 @@
 package ru.sber_tech.prod_mobile.utils
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
