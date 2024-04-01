bbf836790fbaac3ebafa822b9f7bbfe8f77ed160
Ephir <d.y.dvoryannikov@mail.ru>
2024-04-01 10:00:54 +0300

    feat: Improvements AddMeetScreen

--- app/src/main/java/ru/sber_tech/prod_mobile/screens/addMeetScreen/AddMeetScreen.kt
+++ app/src/main/java/ru/sber_tech/prod_mobile/screens/addMeetScreen/AddMeetScreen.kt
@@ -2,19 +2,21 @@ package ru.sber_tech.prod_mobile.screens.addMeetScreen
 
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.koin.androidx.compose.koinViewModel
+import ru.sber_tech.data.addMeetScreen.ErrorState.*
 import ru.sber_tech.domain.addMeetScreen.AddMeetState.*
 import ru.sber_tech.prod_mobile.R.drawable
 import ru.sber_tech.prod_mobile.components.SegmentedButtonSelect
@@ -33,7 +35,8 @@ fun AddMeetScreen(navController: NavController) {
     })
     
     when (val uiState = viewModel.addMeetState.collectAsState().value) {
-        is Adding -> Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        is Adding         -> Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            val errorState = viewModel.errorState.collectAsState()
             Box(contentAlignment = Alignment.Center) {
                 YandexMap()
                 Image(
@@ -42,22 +45,45 @@ fun AddMeetScreen(navController: NavController) {
                     Modifier.size(20.dp)
                 )
             }
-            SegmentedButtonSelect(
-                selectedElements = uiState.model.selectedEvents,
-                options = listOf("adasdsad", "asdasd", "sdfsdfds")
-            ) {
-                viewModel.addOrDeleteElement(it)
+            Row {
+                if (errorState.value == EventError) {
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .width(20.dp)
+                            .height(20.dp)
+                            .clip(CircleShape)
+                            .background(Color.Red)
+                    ) {
+                        Text(
+                            text = "!",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center),
+                            color = Color.White
+                        )
+                    }
+                }
+                SegmentedButtonSelect(
+                    selectedElements = uiState.model.selectedEvents,
+                    options = listOf("adasdsad", "asdasd", "sdfsdfds")
+                ) {
+                    viewModel.addOrDeleteElement(it)
+                }
             }
             PickDateDialog(onConfirm = {
                 viewModel.setDate(it)
-            })
+            }, isSuccessful = errorState.value == DateError)
             PickTimeDialog(onConfirm = {
                 viewModel.setTime(it)
-            })
-            Button(onClick = {
-                viewModel.publish()
-                navController.popBackStack()
-            }) {
+            }, isSuccessful = errorState.value == TimeError)
+            Button(
+                onClick = {
+                    viewModel.publish(onSuccess = {
+                        navController.popBackStack()
+                    })
+                },
+                enabled = uiState.model.date != "" && uiState.model.time != "" && uiState.model.selectedEvents.isNotEmpty(),
+            ) {
                 Text(text = "Готово")
             }
         }
@@ -70,8 +96,8 @@ fun AddMeetScreen(navController: NavController) {
             )
         }
         
-        is Loading -> Box(modifier = Modifier.fillMaxSize()) {
-            CircularProgressIndicator()
+        is Loading        -> Box(modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator(modifier=Modifier.align(Alignment.Center))
         }
     }
 }
@@ -79,11 +105,29 @@ fun AddMeetScreen(navController: NavController) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PickDateDialog(onConfirm: (String) -> Unit) {
+fun PickDateDialog(onConfirm: (String) -> Unit, isSuccessful: Boolean) {
     var openDialog by remember { mutableStateOf(false) }
-    
-    Button(onClick = { openDialog = true }) {
-        Text(text = "Выбрать дату")
+    Row {
+        if (isSuccessful) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .width(20.dp)
+                    .height(20.dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
+            ) {
+                Text(
+                    text = "!",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.Center),
+                    color = Color.White
+                )
+            }
+        }
+        Button(onClick = { openDialog = true }) {
+            Text(text = "Выбрать дату")
+        }
     }
     
     if (openDialog) {
@@ -126,11 +170,29 @@ fun PickDateDialog(onConfirm: (String) -> Unit) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PickTimeDialog(onConfirm: (String) -> Unit) {
+fun PickTimeDialog(onConfirm: (String) -> Unit, isSuccessful: Boolean) {
     var openDialog by remember { mutableStateOf(false) }
-    
-    Button(onClick = { openDialog = true }) {
-        Text(text = "Выбрать время")
+    Row {
+        if (isSuccessful) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .width(20.dp)
+                    .height(20.dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
+            ) {
+                Text(
+                    text = "!",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.Center),
+                    color = Color.White
+                )
+            }
+        }
+        Button(onClick = { openDialog = true }) {
+            Text(text = "Выбрать время")
+        }
     }
     
     if (openDialog) {
--- app/src/main/java/ru/sber_tech/prod_mobile/screens/addMeetScreen/AddMeetScreenViewModel.kt
+++ app/src/main/java/ru/sber_tech/prod_mobile/screens/addMeetScreen/AddMeetScreenViewModel.kt
@@ -1,23 +1,34 @@
 package ru.sber_tech.prod_mobile.screens.addMeetScreen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import ru.sber_tech.data.addMeetScreen.ErrorState
+import ru.sber_tech.data.addMeetScreen.ErrorState.*
 import ru.sber_tech.domain.addMeetScreen.AddMeetModel
 import ru.sber_tech.domain.addMeetScreen.AddMeetState
 import ru.sber_tech.domain.addMeetScreen.AddMeetState.Adding
 import ru.sber_tech.domain.addMeetScreen.AddMeetState.Loading
+import ru.sber_tech.domain.addMeetScreen.AddMeetUseCase
+import ru.sber_tech.domain.addMeetScreen.MeetStatus.ERROR_ON_RECEIPT
+import ru.sber_tech.domain.addMeetScreen.MeetStatus.SUCCESS
 
-class AddMeetScreenViewModel : ViewModel() {
+class AddMeetScreenViewModel(private val addMeetUseCase: AddMeetUseCase) : ViewModel() {
     
     private val _addMeetState = MutableStateFlow<AddMeetState>(Loading)
     val addMeetState = _addMeetState.asStateFlow()
+    
+    private val _errorState = MutableStateFlow<ErrorState?>(null)
+    val errorState = _errorState.asStateFlow()
     
     fun loadElements() {
         _addMeetState.value = Adding(model = AddMeetModel(emptyList(), "", ""))
     }
     
     fun addOrDeleteElement(element: String) {
+        _errorState.value = null
         if (addMeetState.value is Adding) {
             val addingState = addMeetState.value as Adding
             if (element in addingState.model.selectedEvents) {
@@ -34,25 +45,47 @@ class AddMeetScreenViewModel : ViewModel() {
         }
     }
     
-    fun setDate(date: String){
-        if(addMeetState.value is Adding){
+    fun setDate(date: String) {
+        _errorState.value = null
+        if (addMeetState.value is Adding) {
             val addingState = addMeetState.value as Adding
-            _addMeetState.value = addingState.copy(model = addingState.model.copy(
-                date = date
-            ))
+            _addMeetState.value = addingState.copy(
+                model = addingState.model.copy(
+                    date = date
+                )
+            )
         }
     }
     
-    fun setTime(time: String){
-        if(addMeetState.value is Adding){
+    fun setTime(time: String) {
+        _errorState.value = null
+        if (addMeetState.value is Adding) {
             val addingState = addMeetState.value as Adding
-            _addMeetState.value = addingState.copy(model = addingState.model.copy(
-                time = time
-            ))
+            _addMeetState.value = addingState.copy(
+                model = addingState.model.copy(
+                    time = time
+                )
+            )
         }
     }
     
-    fun publish(){
-        // use case
+    fun publish(onSuccess: () -> Unit) {
+        if (addMeetState.value is Adding) {
+            val addingState = addMeetState.value as Adding
+            if (addingState.model.date == "") _errorState.value = DateError
+            if (addingState.model.time == "") _errorState.value = TimeError
+            if (addingState.model.selectedEvents.isEmpty()) _errorState.value = EventError
+            
+            if(errorState.value != null){
+                return
+            }
+            
+            viewModelScope.launch {
+                when (addMeetUseCase.execute(addingState.model)) {
+                    SUCCESS          -> onSuccess()
+                    ERROR_ON_RECEIPT -> _errorState.value = ReceiptError
+                }
+            }
+        }
     }
 }
