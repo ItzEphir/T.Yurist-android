55429f444dcc85a3cb44e7f016f10ac087342ec9
Yasashnyy Maxim <max.yas@list.ru>
2024-04-02 00:42:28 +0300

    feat: add operations Repo

--- data/src/main/java/ru/sber_tech/data/operations/OperationsService.kt
+++ data/src/main/java/ru/sber_tech/data/operations/OperationsService.kt
@@ -4,7 +4,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import kotlinx.serialization.json.Json
-import ru.sber_tech.data.searchScreen.AddressDto
 import ru.sber_tech.domain.operations.OperationModel
 
 class OperationsService(
@@ -13,10 +12,18 @@ class OperationsService(
 
     private val json = Json { ignoreUnknownKeys = true }
 
-    suspend fun getOperations(): List<OperationsDto>?{
+    suspend fun getOperations(): List<OperationModel>?{
         return try {
             val response = httpClient.get("http://158.160.114.41:8080/operations")
-            json.decodeFromString<List<OperationsDto>>(response.bodyAsText())
+            json.decodeFromString<List<OperationsDto>>(response.bodyAsText()).map {
+                OperationModel(
+                    name = it.name,
+                    documents = it.documents,
+                    id = it.id,
+                    duration = it.duration,
+                    product = it.product
+                )
+            }
         } catch (e: Exception) {
             null
         }
